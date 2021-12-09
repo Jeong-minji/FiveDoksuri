5fff5ab5651515abde78f21e4b569e3f7b7a4e58
jcy <poiuytasr@gmail.com>
2021-12-09 20:17:52 +0900

    프로토 타입 학습

--- 1st_week/closure/exampleCode.js
+++ 1st_week/closure/exampleCode.js
@@ -1,26 +1,26 @@
 // Example code
 
-// 외부함수 (outter), 내부함수 (inner)
-function outter() {
+// 외부함수 (outer), 내부함수 (inner)
+function outer() {
   function inner() {
-    const title = "coding everybody";
+    const title = 'coding everybody';
     alert(title);
   }
   inner();
 }
 
 // 위와 동일하게 아래와 같이 변경 가능하다.
-function outter() {
+function outer() {
   const inner = function () {
-    const title = "coding everybody";
+    const title = 'coding everybody';
     alert(title);
   };
   inner();
 }
 
-// 클로저 예시 (내수함수에서 외부함수에 있는 지역변수에 접근)
-function outter() {
-  const title = "coding everybody";
+// 클로저 예시 (내부함수에서 외부함수에 있는 지역변수에 접근)
+function outer() {
+  const title = 'coding everybody';
   function inner() {
     alert(title);
   }
@@ -28,12 +28,12 @@ function outter() {
 }
 
 // 소멸된 외부함수 클로저 예시
-function outter() {
-  const title = "coding everybody";
+function outer() {
+  const title = 'coding everybody';
   return function () {
     alert(title);
   };
 }
 
-const inner = outter();
+const inner = outer();
 inner();
